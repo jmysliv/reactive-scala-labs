2a61aeebed3601c36b347179f83ae7fada7e9157
Jakub Myśliwiec <jakub.mysliwiec@swmansion.com>
2021-10-27 18:19:36 +0200

    Added lab4

---
+++ src/main/scala/EShop/lab3/Payment.scala
@@ -0,0 +1,27 @@
+package EShop.lab3
+
+import EShop.lab2.TypedCheckout
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+
+object Payment {
+
+  sealed trait Command
+  case object DoPayment extends Command
+
+  sealed trait Event
+  case object PaymentConfirmed extends Event
+
+}
+
+class Payment(
+  method: String,
+  orderManager: ActorRef[OrderManager.Command],
+  checkout: ActorRef[TypedCheckout.Command]
+) {
+
+  import Payment._
+
+  def start: Behavior[Payment.Command] = ???
+
+}
